55e732cdf4037665722534ea0fbc792a3c7c0ac5
editkhan2 <168637405+editkhan2@users.noreply.github.com>
2024-05-01 14:04:17 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+function playVideo(link) {
+    var videoElement = document.getElementById('video');
+    videoElement.src = link;
+    videoElement.load();
+    videoElement.play();
+}
+
+function playVideo(link) {
+    var videoElement = document.getElementById('video');
+    videoElement.src = link;
+    videoElement.load();
+    videoElement.play();
+}
